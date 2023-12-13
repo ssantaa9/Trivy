79f9f2c84822b0c28cfe88b1e047d6d1f680c44d
Santiago Santa <santsant@bancolombia.com.co>
2023-12-13 09:32:18 -0500

    Modificaciones a Script para escanear las imágenes locales.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 ## Prerrequisitos
 
-1. Ejecutar el script desde linux.
-2. Tener instalado python3.
-3. Tener instalado Trivy.
+1. Usar una distribución linux.
+2. Docker Engine.
+3. Python3.
+4. Trivy.
+5. Ejecutar el script como super usuario o pertenecer al grupo Docker.
 
 ## Ejecutar el script
 
-1. Abrir una terminal o línea de comandos en linux.
+1. Abrir una terminal o línea de comandos.
 2. Navega hasta la ubicación donde está guardado el script `trivy.py`.
 3. Ejecutar el siguiente comando:
 
    ```bash
-   python3 trivy.py --image <nombre_de_la_imagen>
+   python3 trivy.py
--- trivy.py
+++ trivy.py
@@ -1,53 +1,20 @@
 import subprocess   # Para la ejecución de comandos Bash
-from shlex import quote    # Sanitizar entradas
-from re import compile   # Caracteres permitidos como entrada
-import argparse     # Manejo de argumentos
 
-# Función para sanitizar los argumentos y caracteres permitidos
-# def sanitize(input):
-#     input_sanitized = quote(input)  # Escapar caracteres especiales
-#     input_valid = compile(r'^[a-zA-Z0-9.:]*$')  # Acepta letras, números, punto y/o dos puntos.
-#     # Verificar que la entrada solo contenga los caracteres permitidos
-#     if not input_valid.match(input):
-#         raise ValueError("Entrada no válida. Debe contener solo letras, números, punto y/o dos puntos.")
-    
-#     return input_sanitized
-
-# Ejecutar el comando de Trivy y guardar resultado
-def scan_image(img):
-    # Ejecutar el comando de Trivy
-    result = subprocess.run("trivy --format json --quiet image " + img , shell=True, capture_output=True, text=True)
-    # Si no hay salida, es porque la imagen no existe en los repositorios de docker
-    if result.stdout == "":
-        print ("imagen no encontrada.")
-    else:
+# Ejecutar el comando de Trivy y guardar los resultados
+def scan_images(images):
+    # Recorrer la lista de imágenes
+    for img in images:
+        # Ejecutar el comando de Trivy
+        result = subprocess.run("trivy --format json --quiet image " + img , shell=True, capture_output=True, text=True)
         # Guardar el resultado del escaneo en un archivo
-        with open('image_result.json', 'w') as file:
+        with open(img+'_scan_result.json', 'w') as file:
             file.write(result.stdout)
 
-# Sanitizar y escanear
-def scan(img):
-    try:
-        img = quote(img)   # Sanitizar el argumento
-        scan_image(img) # Escanear una imagen
-        
-    except ValueError as e:
-        print(e)
-
 def main():
-    # Definir el parser para agregar argumentos con su respectiva descripción
-    parser = argparse.ArgumentParser(description='Escanea imágenes docker y retorna un archivo json con los resultados.')
-    # Definir la bandera '--image' que tome un argumento
-    parser.add_argument('--image', type=str, help='Nombre de la imagen a escanear.')
-    # Guardar los argumentos
-    args = parser.parse_args()
-    # Verificar que se haya introducido argumentos
-    if not any(vars(args).values()):
-        parser.print_help() # Si no se ingresaron argumentos, imprimir el menú help
-    else:
-        # Acceder al valor de la bandera '--image'
-        if args.image:
-            scan(args.image)    # Sanitizar y escanear
+    # Comando para listar el ID de las imagenes
+    images = subprocess.run("docker images -q", shell=True, capture_output=True, text=True)
+    images = images.stdout.strip().split('\n')  # Poner las imágenes en una lista
+    scan_images(images) # Escanear las imágenes
 
 # Ejecución de script local
 if __name__ == '__main__':
